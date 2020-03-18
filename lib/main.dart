2f6cd32c6d6ff6c6af2cf7ad494b9ce3bcad8ee9
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-17 19:37:35 -0600

    Inicio de proyecto - Diseños

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_designs/src/pages/basico_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Diseños',
+      initialRoute: 'basico',
+      routes: {
+        'basico': (BuildContext context) => BasicoPage()
+      }
     );
   }
 }
