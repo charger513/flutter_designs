03f5c542c8624d9d487646a0cbf2ab6d04e82791
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-17 20:03:30 -0600

    Diseño básico - parte 2

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -10,33 +10,81 @@ class BasicoPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Image(
-            image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: Row(
-              children: <Widget>[
 
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Lago con un puente', style: estiloTitulo),
-                      SizedBox(height: 7.0),
-                      Text('Un lago en Alemania', style: estiloSubtitulo)
-                    ],
-                  ),
-                ),
+          _crearImagen(),
 
-                Icon(Icons.star, color: Colors.red, size: 30.0),
+          _crearTitulo(),
 
-                Text('41', style: TextStyle(fontSize: 20.0))
-              ],
-            ),
-          )
+          _crearAcciones(),
+
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+
         ],
       )
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+    );
+  }
+
+  Widget _crearTitulo() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+      child: Row(
+        children: <Widget>[
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Lago con un puente', style: estiloTitulo),
+                SizedBox(height: 7.0),
+                Text('Un lago en Alemania', style: estiloSubtitulo)
+              ],
+            ),
+          ),
+
+          Icon(Icons.star, color: Colors.red, size: 30.0),
+          Text('41', style: TextStyle(fontSize: 20.0))
+        ],
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(icon: Icons.call, texto: 'Call'),
+        _accion(icon: Icons.near_me, texto: 'Router'),
+        _accion(icon: Icons.share, texto: 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion({IconData icon, String texto}) {
+    return Column(
+      children: <Widget>[
+        Icon(icon, color: Colors.blue, size: 40.0),
+        SizedBox(height: 5.0),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue))
+      ],
+    );
+  }
+
+  Widget _crearTexto() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque justo porttitor, pulvinar justo eu, tristique nisi. Sed quis turpis risus. In finibus mi eget augue maximus mollis. Nam id suscipit ex, vitae pellentesque justo. Ut eu enim mauris. Nunc dictum felis a leo tincidunt, eu aliquam libero dapibus. Pellentesque bibendum sodales mattis. Nulla sit amet iaculis felis. Aenean sed blandit nunc. Sed aliquet id ipsum non condimentum. Donec non dui non leo rhoncus commodo. Vestibulum pulvinar accumsan tortor. Aenean vitae erat a quam congue dignissim. Nunc eu condimentum sem, a rhoncus enim. Nullam quis nisi posuere lacus auctor molestie. Sed eleifend molestie mauris, lobortis ultrices odio interdum eu.',
+        textAlign: TextAlign.justify,
+      ),
     );
   }
 }
